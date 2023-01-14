f372294a99cce4ba098c97db7ab0149844123bb8
kunal <kunaldayal.786.kd@gmail.com>
2023-01-14 18:07:03 +0530

    1st try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly see = new RWOnly();
+        see.setMember(57);
+        int p = see.getMember();
+        System.out.println(p);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int member;
+
+    public int getMember() {
+        return member;
+    }
+
+    public void setMember(int member) {
+        this.member = member;
+    }
 }
